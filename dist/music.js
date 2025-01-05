bdd2e9d8ccbd1c7e18d2c846587f55117faab623
HiedanoAjuu <hiedanoajuu@outlook.com>
2025-01-05 13:17:51 +0800

    Site updated: 2025-01-05 13:17:50

--- dist/music.js
+++ dist/music.js
@@ -4,14 +4,15 @@ const ap = new APlayer({
     autoplay: true,
     theme: "#000",
     mutex: true,
-    lrcType: 0,
+    lrcType: 1,
     audio: [
         {
-            name: "装飾戦 ～ Decoration Battle",
-            artist: "上海アリス幻樂団",
-            url: "http://music.163.com/song/media/outer/url?id=510039.mp3",
+            name: "Flower of Life",
+            artist: "陽花",
+            url: "http://music.163.com/song/media/outer/url?id=744722.mp3",
             cover:
-                "https://p1.music.126.net/zM4G5cgsfQxsgDftD4RJIw==/109951169299510813.jpg",
+                "http://p1.music.126.net/QN94YMkikhvQN4132VnQVw==/109951163516679812.jpg",
+            lrc: "[00:01.810]the sky, the earth, the sun, a wind,\n[00:05.390]and a little rain are necessary\n[00:09.700]but just smile for me any more\n[00:13.400]let's be with me together\n[00:15.410]during summer...\n[00:17.860]the sky, the earth, the sun, a wind,\n[00:21.400]and a little rain are necessary\n[00:24.890]but just smile for me any more\n[00:28.750]let's be with me together\n[00:31.720]during summer...\n[00:36.430]嗚呼、耳元で爽やかに夏の足音\n[00:43.500]空は青く高く\n[00:51.260]嗚呼、大地から立ち込める薫り集めて\n[00:58.690]日傘ふわり開く\n[01:06.390]また出会いと別れ廻る恋の季節\n[01:13.960]恋とは儚く美しく\n[01:21.470]太陽よりも眩しい笑顔の花\n[01:28.750]今こそ此処に咲かせましょう。\n[01:36.940]嗚呼、足許でいたずらに揺れる夏草\n[01:44.480]日差し浴びて強く\n[01:51.990]嗚呼、そよそよと南風運ぶ思い出\n[01:59.670]ひとり胸を焦がす\n[02:07.430]また今年も廻り廻る恋の季節\n[02:14.960]心に芽生えた恋の花\n[02:22.440]太陽よりも眩しい笑顔浴びて\n[02:30.530]今こそ此処に咲かせましょう。\n[02:38.170]the sky, the earth, the sun, a wind,\n[02:41.840]and a little rain are necessary\n[02:45.840]but just smile for me any more\n[02:49.560]let's be with me together\n[02:51.430]during summer...\n[02:53.080]the sky, the earth, the sun, a wind,\n[02:57.090]and a little rain are necessary\n[03:00.710]but just smile for me any more\n[03:04.950]let's be with me together\n[03:06.850]during summer...\n[03:08.510]いつかは終わりの来るその生命(いのち)\n[03:16.120]少しでも永くそばにいて…\n[03:23.350]心に大粒の雨が降った日は\n[03:31.270]抱きしめてキスをして\n[03:38.840]涙をそっと拭いてあげるから…\n[03:49.960]また出会いと別れ廻る恋の季節\n[03:57.790]恋とは儚く美しく\n[04:05.360]太陽よりも眩しい笑顔の花\n[04:12.880]今こそ此処に咲かせましょう。\n[04:20.560]また今年も廻り廻る恋の季節\n[04:28.250]心に芽生えた恋の花\n[04:35.920]太陽よりも眩しい笑顔浴びて\n[04:43.380]今こそ此処に咲かせましょう。\n[04:50.380]the sky, the earth, the sun, a wind,\n[04:56.560]and a little rain are necessary\n[05:01.440]but just smile for me any more\n[05:04.750]let's be with me together\n[05:06.800]during summer...\n[05:10.560]the sky, the earth, the sun, a wind,\n[05:12.420]and a little rain are necessary\n[05:16.020]but just smile for me any more\n[05:19.730]let's be with me together\n[05:21.890]during summer...\n[05:23.870]the sky, the earth, the sun, a wind,\n[05:27.630]and a little rain are necessary\n[05:31.340]but just smile for me any more\n[05:35.200]let's be with me together\n[05:36.960]during summer...",
         }
     ]
 })
